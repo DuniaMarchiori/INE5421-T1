5ee6e4a26ed51b64c30f173516a6b6881409ccdf
Vinicius Steffani Schweitzer <schweitzer.vinicius@gmail.com>
2018-05-08 11:00:44 -0300

    Conserto de bugs e testes preliminares, é possível gerar a arvore de uma expressão simples

--- model/Expressao.py
+++ model/Expressao.py
@@ -5,6 +5,7 @@ from model.Arvore.Nodos.NodoConcat import NodoConcat
 from model.Arvore.Nodos.NodoFecho import NodoFecho
 from model.Arvore.Nodos.NodoOpcional import NodoOpcional
 from enum import Enum
+import string
 
 class Expressao:
 
@@ -27,10 +28,11 @@ class Expressao:
         return texto
 
     def gerarArvore(self, expressao):
-        arvore = Arvore()
+        self.arvore = Arvore()
         if self.verificaValidade(expressao):
-            expressao.replace(" ", "") # remove espaços
-            arvore.setNodoRaiz(self.gerarNodo(expressao))
+            #expressao.replace(" ", "") # Remove espaços da expressao inteira
+            expressao = "".join(expressao.split()) # Remove espaços da expressao inteira
+            self.arvore.setNodoRaiz(self.gerarNodo(expressao))
 
     def verificaValidade(self, expressao):
         # TODO verificar se a expressao contem apenas caracteres do alfabeto, operadores ou espaços em branco
@@ -42,13 +44,8 @@ class Expressao:
             #pass
 
     def gerarNodo(self, expressao):
-        subexpressao = expressao
-        temParenteses = True
-        while (temParenteses):
-            if subexpressao[0] == "(" and subexpressao[len(subexpressao)] == ")":
-                subexpressao = subexpressao[1:-1]
-            else:
-                temParenteses = False
+        subexpressao = self.__removerParentesesExternos(expressao)
+        subexpressao = self.__exporConcatenacoesOcultas(subexpressao)
 
         if len(subexpressao) == 1:
             # TODO verificar se expressão é realmente um caracter terminal
@@ -84,23 +81,51 @@ class Expressao:
                         prioridadeDiv = 0
                         posicaoDiv = i
 
+            nodo = None
             if operadorDiv == Operacao.UNIAO:
                 nodo = NodoUniao()
                 nodo.setFilhoEsquerdo(self.gerarNodo(subexpressao[0:posicaoDiv]))
                 nodo.setFilhoDireito(self.gerarNodo(subexpressao[posicaoDiv+1:]))
 
-            if operadorDiv == Operacao.CONCAT:
+            elif operadorDiv == Operacao.CONCAT:
                 nodo = NodoConcat()
                 nodo.setFilhoEsquerdo(self.gerarNodo(subexpressao[0:posicaoDiv]))
                 nodo.setFilhoDireito(self.gerarNodo(subexpressao[posicaoDiv+1:]))
 
-            if operadorDiv == Operacao.FECHO:
+            elif operadorDiv == Operacao.FECHO:
                 nodo = NodoFecho()
                 nodo.setFilhoEsquerdo(self.gerarNodo(subexpressao))
 
-            if operadorDiv == Operacao.OPCIONAL:
+            else: # operadorDiv == Operacao.OPCIONAL:
                 nodo = NodoOpcional()
                 nodo.setFilhoEsquerdo(self.gerarNodo(subexpressao))
+
+            return nodo
+
+    def __removerParentesesExternos(self, expressao):
+        temParenteses = True
+        while (temParenteses):
+            if expressao[0] == "(" and expressao[-1] == ")":
+                expressao = expressao[1:-1]
+            else:
+                temParenteses = False
+        return expressao
+
+    def __exporConcatenacoesOcultas(self, expressao):
+        novaExpressao = expressao
+        charAnterior = ""
+        concatsAdicionadas = 0
+        for i in range(0, len(expressao)):
+            char = expressao[i]
+            if self.__charPertenceAConcatenacao(charAnterior) and self.__charPertenceAConcatenacao(char):
+                novaExpressao = novaExpressao[:i+concatsAdicionadas] + '.' + novaExpressao[i+concatsAdicionadas:]
+                concatsAdicionadas += 1
+            charAnterior = char
+
+        return novaExpressao
+
+    def __charPertenceAConcatenacao(self, char):
+        return char == ("(" or ")" or "*" or "?") or char.isalnum()
 
 
 class Operacao(Enum):
