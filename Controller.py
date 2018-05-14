fc02f81cc4d2b0b3a48238d314d560c78996b04f
Vinicius Steffani Schweitzer <schweitzer.vinicius@gmail.com>
2018-05-14 14:42:25 -0300

    Começo da estrutura mvc

--- Controller.py
+++ Controller.py
@@ -1,10 +1,33 @@
 from model.Model import Model
+from view.View import View
 
 '''
     Controller do padrão MVC.
 '''
 class Controller:
-    __model = Model() # fachada do modelo
+
+    __model = None  # Fachada do modelo
+    __view = None  # Tela principal da aplicação
+
+    '''
+       Método construtor.
+    '''
+    def __init__(self):
+        self.__model = Model()  # Fachada do modelo
+        self.__view = View()  # Tela principal da aplicação
+        self.__view.start()
+
+    def cb_nova_gramatica(self, valor):
+        pass
+
+    def cb_nova_expressao(self, valor):
+        pass
+
+
+
+
+
+
 
     '''
         Salva uma gramática em um arquivo.
