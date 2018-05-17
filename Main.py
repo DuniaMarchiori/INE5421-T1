c0002a29919512cd9da2d32a1f4433daa9b722f7
Dúnia <DuniaMarchiori@users.noreply.github.com>
2018-05-17 11:35:18 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,25 +1,12 @@
 from Controller import Controller
-from model.Gramatica import Gramatica
-from model.exception.FormatError import FormatError
+from model.AF.Estado import Estado
 
 
 def main():
     c = Controller()
-    texto = "S ->  0B|0S| 1  B| & \n  B->0"
-    g = Gramatica()
-    try:
-        g.parse(texto)
-    except FormatError as err:
-        print(err.get_message())
-
-    af = g.transformar_em_AF()
-    for l in af.to_string():
-        print(l)
-    af2 = af.determiniza()
-    #af2.printa()
-    for l in af2.to_string():
-        print(l)
-    '''
+    texto = "S - > 0B| 0S| 1  B|  & \n  B->0"
+    arquivo = c.salvar_gramatica(texto)
+    g = c.abrir_gramatica(arquivo)
     #g.printa()
     #print(g.to_string())
     af = c.transformar_GR_em_AF(g)
@@ -30,11 +17,10 @@ def main():
         print(l)
     g2 = c.transformar_AF_em_GR(af)
     g2.printa()
-    print(g2.to_string())'''
+    print(g2.to_string())
 
-    '''texto = "(ab|ac)* a% | (ba?c)*"
-    e = Expressao(texto)
-    afe = e.obter_automato_finito_equivalente()
-    afe.printa()'''
+    texto = "a*"
+    arquivo = c.salvar_expressao(texto)
+    c.abrir_expressao(arquivo)
 
 main()
